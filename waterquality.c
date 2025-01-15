78d7d2d7805ca30bd13f49e92ad26e600459c17f
DCN-LAB <theduelist111103@gmail.com>
2025-01-15 23:35:20 +0530

    waterquality.c

--- waterquality.c
+++ waterquality.c
@@ -1,62 +1,50 @@
-#define TRIG_PIN 27
-#define ECHO_PIN 26
-#define ADC_PIN 35
-#define BUZZER_PIN 23
-
-// Constants
-const int NUM_READINGS = 5;
-const float EMPTY_LEVEL = 14.0;
-const float FULL_LEVEL = 3.0;
-const int CLEAR_THRESHOLD = 50;
-const int BLOCKED_THRESHOLD = 4095;
+#define TRIG 8
+#define ECHO 9
+#define TURBIDITY_PIN A0
+#define LED_PIN 10
+#define BUZZER_PIN 11
+#define DIST_THRESHOLD 20
+#define TURB_THRESHOLD 400
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(ADC_PIN, INPUT);
-  pinMode(BUZZER_PIN, OUTPUT);
+    Serial.begin(9600);
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+    pinMode(TURBIDITY_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
 }
 
 void loop() {
-  // Read water level and turbidity
-  float distance = getDistance();
-  int turbidityValue = analogRead(ADC_PIN);
-  int turbidityPercentage = map(turbidityValue, CLEAR_THRESHOLD, BLOCKED_THRESHOLD, 100, 0); // Normalize turbidity value
-
-  // Print status to Serial Monitor
-  Serial.print("Water Level: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  Serial.print("Turbidity: ");
-  Serial.print(turbidityPercentage);
-  Serial.println(" %");
-
-  // Handle warnings with buzzer
-  if (distance > EMPTY_LEVEL || turbidityPercentage > 50) { // Buzzer for high turbidity or empty water level
-    tone(BUZZER_PIN, 1000, 250);  
-    delay(500);
-  } else {
-    noTone(BUZZER_PIN);
-  }
-
-  delay(500);
-}
-
-float getDistance() {
-  long totalDuration = 0;
-
-  for (int i = 0; i < NUM_READINGS; i++) {
-    digitalWrite(TRIG_PIN, LOW);
+    // Ultrasonic sensor distance
+    digitalWrite(TRIG, LOW);
     delayMicroseconds(2);
-    digitalWrite(TRIG_PIN, HIGH);
+    digitalWrite(TRIG, HIGH);
     delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
+    digitalWrite(TRIG, LOW);
+    long duration = pulseIn(ECHO, HIGH);
+    float distance = (duration * 0.034) / 2;
 
-    totalDuration += pulseIn(ECHO_PIN, HIGH);
-    delay(50);
-  }
+    // Turbidity sensor value
+    int turbidity = analogRead(TURBIDITY_PIN);
 
-  return (totalDuration / NUM_READINGS) * 0.034 / 2; // Average distance in cm
+    // Print data
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    Serial.print("Turbidity: ");
+    Serial.println(turbidity);
+
+    // Monitor water quality and create alerts
+    if (distance > DIST_THRESHOLD || turbidity > TURB_THRESHOLD) {
+        digitalWrite(LED_PIN, HIGH);
+        digitalWrite(BUZZER_PIN, HIGH);
+    } else {
+        digitalWrite(LED_PIN, LOW);
+        digitalWrite(BUZZER_PIN, LOW);
+    }
+
+    delay(2000);
 }
