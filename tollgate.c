060e0543cd4a2efd494b06b63aee267c326e7bf2
DCN-LAB <theduelist111103@gmail.com>
2025-01-15 23:26:45 +0530

    tollgate.c

--- tollgate.c
+++ tollgate.c
@@ -1,14 +1,14 @@
 #include <Servo.h>
 
 // Define Ultrasonic Sensor Pins
-#define TRIG_PIN 27
-#define ECHO_PIN 26
+#define TRIG_PIN 7
+#define ECHO_PIN 6
 
 // Define IR Sensor Pin
-#define IR_SENSOR_PIN 34
+#define IR_SENSOR_PIN 2
 
 // Define Servo Motor Pin
-#define SERVO_PIN 25
+#define SERVO_PIN 9
 
 Servo servo;
 
@@ -16,7 +16,8 @@ Servo servo;
 #define DISTANCE_THRESHOLD 10 // in cm
 
 void setup() {
-    Serial.begin(115200);
+    // Initialize Serial Communication
+    Serial.begin(9600);
 
     // Configure IR Sensor
     pinMode(IR_SENSOR_PIN, INPUT);
@@ -36,13 +37,13 @@ void loop() {
     // Vehicle Detection (Choose either IR or Ultrasonic)
     bool vehicleDetected = false;
 
-    // IR Sensor
+    // IR Sensor Detection
     if (digitalRead(IR_SENSOR_PIN) == HIGH) {
         vehicleDetected = true;
         Serial.println("Vehicle Detected (IR Sensor)");
     }
 
-    // Ultrasonic Sensor
+    // Ultrasonic Sensor Detection
     long duration;
     float distance;
 
